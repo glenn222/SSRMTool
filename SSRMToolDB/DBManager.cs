b583d27755a73fbfc9a7e346813fef797e3c0aea
Tuba Opel <tubaopel@gmail.com>
2017-03-31 17:29:30 -0400

    Add more files to database stuff

---
+++ SSRMToolDB/DBManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+using ServiceStack.Text;
+using SSRMTool;
+
+
+namespace SSRMToolDB
+{
+    class DBManager
+    {   
+        private RedisClient redis;
+        private List<String> staircaseKeys;
+        private List<Staircase> staircases;
+
+        private DBManager()
+        {
+            redis = new RedisClient();
+            using (redis)
+            {
+                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
+                staircaseKeys = redisUsers.GetAllKeys();
+                staircases = redisUsers.GetAll().ToList();
+            }
+        }
+
+        public DBManager GetInstance()
+        {
+            if (this == null)
+            {
+                DBManager doc = new DBManager();
+                return doc;
+            }
+            else
+                return this;
+        }
+
+        private List<String> StaircaseKeys
+        {
+            get { return staircaseKeys; }
+        }
+
+        private List<Staircase> Staircases
+        {
+            get { return staircases; }
+        }
+
+        public void ModifyStaircaseInDB(Staircase obj)
+        {
+            using (redis)
+            {
+                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
+                redisUsers.GetAndSetValue(obj.StaircaseName,obj);
+            }
+        }
+        public void DeleteStaircaseFromDB(Staircase obj)
+        {
+            using (redis)
+            {
+                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
+                redisUsers.RemoveEntry(obj.StaircaseName);
+            }
+        }
+
+        public void AddStaircaseInDB(Staircase obj)
+        {
+            using (redis)
+            {
+                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
+                redisUsers.GetAndSetValue(obj.StaircaseName, obj);
+            }
+
+        }
+
+       
+    }
+}
--- SSRMToolDB/DocumentManager.cs
+++ SSRMToolDB/DocumentManager.cs
@@ -3,79 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ServiceStack.Redis;
-using ServiceStack.Redis.Generic;
-using ServiceStack.Text;
-using SSRMTool;
-
 
 namespace SSRMToolDB
 {
     class DocumentManager
-    {   
-        private RedisClient redis;
-        private List<String> staircaseKeys;
-        private List<Staircase> staircases;
+    {
 
-        private DocumentManager()
-        {
-            redis = new RedisClient();
-            using (redis)
-            {
-                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
-                staircaseKeys = redisUsers.GetAllKeys();
-                staircases = redisUsers.GetAll().ToList();
-            }
-        }
 
-        public DocumentManager GetInstance()
-        {
-            if (this == null)
-            {
-                DocumentManager doc = new DocumentManager();
-                return doc;
-            }
-            else
-                return this;
-        }
-
-        private List<String> StaircaseKeys
-        {
-            get { return staircaseKeys; }
-        }
-
-        private List<Staircase> Staircases
-        {
-            get { return staircases; }
-        }
-
-        public void ModifyStaircaseInDB(Staircase obj)
-        {
-            using (redis)
-            {
-                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
-                redisUsers.GetAndSetValue(obj.StaircaseName,obj);
-            }
-        }
-        public void DeleteStaircaseFromDB(Staircase obj)
-        {
-            using (redis)
-            {
-                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
-                redisUsers.RemoveEntry(obj.StaircaseName);
-            }
-        }
-
-        public void AddStaircaseInDB(Staircase obj)
-        {
-            using (redis)
-            {
-                IRedisTypedClient<Staircase> redisUsers = redis.As<Staircase>();
-                redisUsers.GetAndSetValue(obj.StaircaseName, obj);
-            }
-
-        }
-
-       
     }
 }
