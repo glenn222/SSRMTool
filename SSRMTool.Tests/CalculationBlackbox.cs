6fb1f93203d34032d904152bb69a65c8a220237b
Oberon Dixon-Luinenburg <oberondixon@gmail.com>
2017-03-31 18:00:57 -0400

    New tests

---
+++ SSRMTool.Tests/CalculationBlackbox.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using NCalc;
+using System.Diagnostics;
+
+namespace SSRMTool.Tests
+{
+    [TestClass]
+    public class CalculationBlackbox
+    {
+        [TestMethod]
+        public void BuildEmptyFunction()
+        {
+            List<double> indep = new List<double>(new double[] { });
+            List<double> dep = new List<double>(new double[] { });
+            string output = SSRMTool.Staircase.PiecewiseFit(indep, dep, "[x]", "1e6", "tanh", "exp");
+            Console.WriteLine(output);
+            bool isempty = (output == "");
+            Assert.IsTrue(isempty);
+        }
+        [TestMethod]
+        public void BuildNon_EmptyFunction()
+        {
+            List<double> indep = new List<double>(new double[] { 1, 2 });
+            List <double> dep = new List<double>(new double[] { 1, 10 });
+            string output = SSRMTool.Staircase.PiecewiseFit(indep, dep, "[x]", "1e6", "tanh", "exp");
+            Console.WriteLine(output);
+            bool matches = (output == "exp(2.30258509299405*[x]+(-2.30258509299405))*0.5*(1+tanh(-1e6*([x]-(2))))");
+            Assert.IsTrue(matches);
+        }
+        [TestMethod]
+        public void ExpressionOutputMatching()
+        {
+            List<double> indep = new List<double>(new double[] { 1, 2, 3, 4, 5 });
+            List<double> dep = new List<double>(new double[] { 1, 10, 100, 1000, 10000 });
+
+            string output = SSRMTool.Staircase.PiecewiseFit(indep, dep, "[x]");
+            Expression function = new Expression(output);
+            bool vals_match = true;
+            double val;
+            for (int i = 0; i < indep.Count; i++)
+            {
+                function.Parameters["x"] = indep[i];
+                val = (double)function.Evaluate();
+                Console.WriteLine(val);
+                vals_match = vals_match && (val - dep[i] < 0.001);
+            }
+            
+            Assert.IsTrue(vals_match);
+        }
+        [TestMethod]
+        public void RuntimeAcceptance()
+        {
+            List<double> indep = new List<double>(new double[] { 1, 2, 3, 4, 5 });
+            List<double> dep = new List<double>(new double[] { 1, 10, 100, 1000, 10000 });
+            string output = SSRMTool.Staircase.PiecewiseFit(indep, dep, "[x]");
+            Expression function = new Expression(output);
+            Stopwatch timer = new Stopwatch();
+            int operations = 262144;
+            timer.Start();
+            for (int i = 0; i < operations; i++)
+            {
+                function.Parameters["x"] = 1;
+                function.Evaluate();
+            }
+            timer.Stop();
+            Console.WriteLine("Time for 1 million calculations (1000x1000 image): " + timer.ElapsedMilliseconds + "ms");
+            bool test_time = timer.ElapsedMilliseconds < 30000;
+            Assert.IsTrue(test_time);
+        }
+    }
+}
---
+++ SSRMTool.Tests/StaircaseWhitebox.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NCalc;
+
+namespace SSRMTool.Tests
+{
+    [TestClass]
+    public class StaircaseWhitebox
+    {
+        [TestMethod]
+        public void AddLiterature()
+        {
+            List<double> rho = new List<double>(new double[] { 1, 10, 100, 1000 });
+            List<double> cm3 = new List<double>(new double[] { 1, 10, 100, 1000 });
+            Staircase test = new Staircase(5,"ST3","n-type doped","Si");
+            test.DefineSteps(rho, cm3);
+            bool type = test.LiteratureResistivity[0].GetType() == typeof(double) && test.LiteratureCarriers[0].GetType() == typeof(double);
+            bool length = test.LiteratureResistivity.Count == 4 && test.LiteratureCarriers.Count == 4;
+            bool meas = test.Measurements.Count == 0;
+            bool metadata = test.StaircaseName == "ST3" && test.StaircaseMaterial == "Si" && test.StaircaseDescription == "n-type doped";
+            Assert.IsTrue(type&&length&&meas&&metadata);
+        }
+        [TestMethod]
+        public void AddMeasurement()
+        {
+            List<double> rho = new List<double>(new double[] { 1, 10, 100, 1000 });
+            List<double> cm3 = new List<double>(new double[] { 1, 10, 100, 1000 });
+            List<double> R = new List<double>(new double[] { 1, 10, 100, 1000 });
+            List<double> dR = new List<double>(new double[] { 1, 10, 100, 1000 });
+            Staircase test = new Staircase(5, "ST3", "n-type doped", "Si");
+            test.DefineSteps(rho, cm3);
+            test.AddMeas("A","B","C",R, dR);
+            bool length = test.Measurements.Count == 1;
+            bool type = test.Measurements[0].GetType() == typeof(Measurement);
+            bool steps_meas = test.Measurements[0].Resistance.Count == 4 && test.Measurements[0].ResistanceAmplitude.Count == 4;
+            bool metadata = test.Measurements[0].Tip == "A" && test.Measurements[0].Date == "B" && test.Measurements[0].Description == "C";
+            Assert.IsTrue(length&&type&&steps_meas&&metadata);
+        }
+        [TestMethod]
+        public void PopulateFunctions()
+        {
+            List<double> rho = new List<double>(new double[] { 1, 10, 100, 1000 });
+            List<double> cm3 = new List<double>(new double[] { 1, 10, 100, 1000 });
+            List<double> R = new List<double>(new double[] { 1, 10, 100, 1000 });
+            List<double> dR = new List<double>(new double[] { 1, 10, 100, 1000 });
+            Staircase test = new Staircase(5, "ST3", "n-type doped", "Si");
+            test.DefineSteps(rho, cm3);
+            test.AddMeas("", "", "", R, dR);
+            test.BuildFunctions(0);
+            bool type = test.Measurements[0].Functions.GetType() == typeof(Expression[]);
+            bool length = test.Measurements[0].Functions.Length == 4;
+            Assert.IsTrue(length && type);
+        }
+
+    }
+}
