baeca82ed5f9a03a700e36e38b05d564e793e692
Glendon Ngo <g2ngo@uwaterloo.ca>
2017-04-01 15:27:29 -0400

    Add Unit Tests for DB Layer, Remove extra projects

---
+++ SSRMToolDB.Tests/DBManagerUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SSRMToolDB.Tests
+{
+    [TestClass]
+    public class DBManagerUnitTests
+    {
+        [TestMethod]
+        public void Add_Staircase_Object()
+        {
+
+        }
+    }
+}
