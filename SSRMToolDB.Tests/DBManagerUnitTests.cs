850d70bf010033fd5af0ed5e98b433e19d805ad3
Glendon Ngo <g2ngo@uwaterloo.ca>
2017-04-01 16:50:10 -0400

    Add staircase to db unit test

--- SSRMToolDB.Tests/DBManagerUnitTests.cs
+++ SSRMToolDB.Tests/DBManagerUnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SSRMTool;
 
 namespace SSRMToolDB.Tests
 {
@@ -9,7 +10,19 @@ namespace SSRMToolDB.Tests
         [TestMethod]
         public void Add_Staircase_Object()
         {
+            
+            var dbManager = new DocumentManager();
+            var stairCase = new Staircase(1, "StaircaseName", "A Staircase", "Some Material");
+            
+            dbManager.addStaircase(stairCase);
 
+            var dbStaircase = dbManager.queryStaircase(stairCase.StaircaseName);
+
+            Assert.AreEqual(stairCase.StaircaseName, dbStaircase.StaircaseName);
+
+            Assert.IsTrue(dbManager.staircases.Contains(dbStaircase));
         }
+
+
     }
 }
